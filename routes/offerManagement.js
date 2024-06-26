5230f96245411ed1780fc701da2a0abfe8589c92
4o4fra <franio.grzywaczewski@gmail.com>
2024-06-26 17:48:59 +0200

    fix to remove offers on all accounts

--- routes/offerManagement.js
+++ routes/offerManagement.js
@@ -5,19 +5,20 @@ const path = require("node:path");
 
 let data = JSON.parse(fs.readFileSync(path.join(__dirname, '../data.json'), 'utf8'));
 const config = JSON.parse(fs.readFileSync(path.join(__dirname, '../config.json'), 'utf8'));
+const shippingRatesIds = JSON.parse(fs.readFileSync(path.join(__dirname, '../shippingRatesIds.json'), 'utf8'));
 
-fs.watch(path.join(__dirname, '../data.json'), (eventType, filename) => {
+fs.watch(path.join(__dirname, '../data.json'), (eventType) => {
     if (eventType === 'change') {
         data = JSON.parse(fs.readFileSync(path.join(__dirname, '../data.json'), 'utf8'));
         console.log('data.json has been updated, data refreshed.');
     }
 });
 
-async function getOffers(data, shippingRatesId) {
+async function getOffers(data) {
     const results = [];
     for (let account of data) {
         const bearerToken = account.access_token;
-
+        const shippingRatesId = shippingRatesIds[account.id] ?? '';
         try {
             const response = await axios.get('https://api.allegro.pl/sale/offers', {
                 headers: {
@@ -49,8 +50,8 @@ async function getOffers(data, shippingRatesId) {
     return results;
 }
 
-async function getOfferWithCertainStock(stockCount, shippingRatesId) {
-    const results = await getOffers(data, shippingRatesId);
+async function getOffersWithCertainStock(stockCount) {
+    const results = await getOffers(data);
     let filteredOffers = [];
     results.forEach((result, index) => {
         result.data.offers.forEach(offer => {
@@ -66,20 +67,17 @@ async function getOfferWithCertainStock(stockCount, shippingRatesId) {
 }
 
 router.get('/', async (req, res) => {
-    const shippingRatesId = req.query.shippingRatesId ?? null;
-    const results = await getOffers(data, shippingRatesId);
+    const results = await getOffers(data);
     res.json(results);
 });
 
 router.get('/:stockCount', async (req, res) => {
-    const offers = await getOfferWithCertainStock(req.params.stockCount, req.query.shippingRatesId);
+    const offers = await getOffersWithCertainStock(req.params.stockCount);
     res.status(200).json(offers);
 });
 
 setInterval(async () => {
-    const {stockCount, shippingRatesId} = config;
-    const offers = await getOfferWithCertainStock(stockCount, shippingRatesId);
-
+    const offers = await getOffersWithCertainStock(config.stockCount);
     for (let offer of offers) {
         try {
             const response = await axios.patch(`https://api.allegro.pl/sale/product-offers/${offer.id}`, {
